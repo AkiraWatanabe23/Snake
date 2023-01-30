1b84abb9cbcd1f7983ea1efe7f0556699d273187
Akira Watanabe <a0323w@gmail.com>
2023-01-30 19:55:24 +0900

    [update] クリックした位置にCubeを出す

--- Assets/Scripts/System/Consts.cs
+++ Assets/Scripts/System/Consts.cs
@@ -1,9 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Constants
 {
     public static class Consts
     {
+        public const int SET_LIMIT = 15;
         public const float GAME_TIME = 100f;
+
+        public static readonly Dictionary<SceneNames, string> Scenes = new()
+        {
+            [SceneNames.TITLE_SCENE] = "TitleScene",
+            [SceneNames.GAME_SCENE] = "GameScene",
+            [SceneNames.RESULT_SCENE] = "ResultScene",
+        };
+    }
+
+    public enum SceneNames
+    {
+        TITLE_SCENE,
+        GAME_SCENE,
+        RESULT_SCENE,
     }
 }
---
+++ Assets/Scripts/System/SetBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBox : MonoBehaviour
+{
+    [SerializeField] private GameObject _setObj = default;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        //左クリックで生成
+        if (Input.GetMouseButtonDown(0))
+        {
+            var inputPos = Input.mousePosition;
+            inputPos.z = 10f;
+            Instantiate(_setObj, Camera.main.ScreenToWorldPoint(inputPos), Quaternion.identity);
+        }
+    }
+}
