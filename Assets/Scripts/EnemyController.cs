ba569b1b433650a119bcdc45819fb0308919531a
Akira Watanabe <a0323w@gmail.com>
2023-01-30 18:57:51 +0900

    initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+    [Tooltip("進行可能先")]
+    [SerializeField] private List<Transform> _pos = new();
+
+    private NavMeshAgent _agent = default;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (_agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            //設定した地点に着いたら、次の進行先を設定する
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
