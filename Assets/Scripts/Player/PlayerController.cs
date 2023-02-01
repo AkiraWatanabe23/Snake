40eb0bdb376f4fbb5d19333f2bf9c6828c37ee26
Akira Watanabe <a0323w@gmail.com>
2023-02-01 20:45:20 +0900

    [add] Playerの移動(修正箇所有)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float _moveSpeed = 1f;
+
+    private Rigidbody _rb = default;
+    private Vector3 _moveDir = default;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        float hol = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        _moveDir = Vector3.forward * ver + Vector3.right * hol;
+        _moveDir = Camera.main.transform.TransformDirection(_moveDir);
+        _moveDir.y = 0f;
+
+        Vector3 forward = _rb.velocity;
+        forward.y = 0;
+
+        if (forward != Vector3.zero)
+        {
+            transform.forward = forward;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(_moveDir.normalized * _moveSpeed - _rb.velocity);
     }
 }
